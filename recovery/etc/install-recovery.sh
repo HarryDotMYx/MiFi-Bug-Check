45db1d4f3076892057357fc395adde841c975359
PG Mohd Azhan Fikri <90368539+HarryDotMYx@users.noreply.github.com>
2025-06-30 13:19:00 +0800

    Ni Upgrade Baru
    Upgrade baru haha

--- recovery/etc/install-recovery.sh
+++ recovery/etc/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c MTD:recovery:10827776:a9bb74703e92b5fa76a4a0b20918306e38933a92; then
-  applypatch  MTD:boot:10827776:a9bb74703e92b5fa76a4a0b20918306e38933a92 MTD:recovery a9bb74703e92b5fa76a4a0b20918306e38933a92 10827776 a9bb74703e92b5fa76a4a0b20918306e38933a92:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c MTD:recovery:10913792:01d1cc69625ed7b44e7a3867930307331c6fa27a; then
+  applypatch  MTD:boot:10913792:01d1cc69625ed7b44e7a3867930307331c6fa27a MTD:recovery 01d1cc69625ed7b44e7a3867930307331c6fa27a 10913792 01d1cc69625ed7b44e7a3867930307331c6fa27a:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
